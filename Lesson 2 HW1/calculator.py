ff391c60d97216fd46566759944231ee9ab42543
Elif Nur Kanat <elifnrkanat@gmail.com>
2023-09-21 13:50:44 +0300

    Create calculator.py

---
+++ Lesson 2 HW1/calculator.py
@@ -0,0 +1,20 @@
+number1 = int(input("1.Sayıyı giriniz: "))
+
+number2 = int(input("2.Sayıyı giriniz: "))
+
+operation = input("Yapmak istediğiniz işlemi giriniz: ")
+
+if(operation == "+" ):
+    print(number1, "+", number2, "= ", number1 + number2)
+
+elif(operation == "-" ):
+    print(number1, "-", number2, "= ", number1 - number2)
+  
+elif(operation == "*"):
+    print(number1, "*", number2, "= ", number1 * number2)
+  
+elif(operation == "/"):
+    print(number1, "/", number2, "= ", number1 / number2)
+  
+else:
+    print("Hatalı giriş yaptınız..")
